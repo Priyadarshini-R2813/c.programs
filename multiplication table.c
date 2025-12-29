915a982ddaa5879535f93ec8e503817f1930b29e
priyadarshini.R <priyaaramesh007@gmail.com>
2025-12-29 19:43:38 +0530

    Add files via upload

---
+++ multiplication table.c
@@ -0,0 +1,12 @@
+//c program to print multiplication table using do while loop
+#include <stdio.h>
+int main() {
+    int num, i = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    do {
+        printf("%d x %d = %d\n", num, i, num * i);
+        i++;
+    } while(i <= 10);
+    return 0;
+}
